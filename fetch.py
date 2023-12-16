ad09cb176c993bf02cfaff088f59656a825cc3f6
sdcharly <142165510+sdcharly@users.noreply.github.com>
2023-12-16 15:19:19 +0530

    Update fetch.py

--- fetch.py
+++ fetch.py
@@ -1,72 +1,70 @@
 import os
 from flask import Flask, request, render_template
-from pathlib import Path
-import requests
 from werkzeug.utils import secure_filename
+import requests
+
+app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = 'uploads'
+app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024  # 16MB upload limit
 
 ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg', 'gif'}
 
 # Assuming an environment variable 'GEMINI_API_KEY' is set in render.com
-api_key = os.environ.get('GEMINI_KEY')
+api_key = os.environ.get('GEMINI_API_KEY')
 
-app = Flask(__name__)
+# Input prompt for understanding invoices
+input_prompt = """
+               You are an expert in understanding invoices.
+               You will receive input images as invoices &
+               you will have to answer questions based on the input image
+               """
 
 def allowed_file(filename):
-    return '.' in filename and \
-           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
     if request.method == 'POST':
         file = request.files['file']
-        question_prompt = request.form.get('question')
+        question_prompt = request.form['question']
 
         if file and allowed_file(file.filename) and question_prompt:
             filename = secure_filename(file.filename)
-            file_path = save_file(file, filename)
+            file_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+            file.save(file_path)
+
             response_text = generate_gemini_response(file_path, question_prompt)
             return render_template('index.html', response=response_text)
-        else:
-            return render_template('index.html', error="Invalid file type. Only image files are allowed.")
 
     return render_template('index.html')
 
-def save_file(file, filename):
-    file_path = os.path.join('uploads', filename)
-    file.save(file_path)
-    return file_path
-
 def generate_gemini_response(image_loc, question_prompt):
-    image_prompt = input_image_setup(image_loc)
-
-    prompt_parts = [image_prompt, question_prompt]
-    response = model_generate_content(prompt_parts)
-    return response.text
-
-def input_image_setup(file_loc):
-    if not (img := Path(file_loc)).exists():
-        raise FileNotFoundError(f"Could not find image: {img}")
-
-    image_parts = {
+    image_data = {
         "mime_type": "image/jpeg",
-        "data": Path(file_loc).read_bytes()
+        "data": open(image_loc, 'rb').read()
     }
-    return image_parts
+    
+    prompt_parts = [input_prompt, image_data, {"text": question_prompt}]
+    response = model_generate_content(prompt_parts)
+    return response.text if response else "Error in response"
 
 def model_generate_content(prompt_parts):
-    # Replace with appropriate API endpoint and parameters
-    api_endpoint = "//uploads"
+    # Replace with the appropriate API endpoint and parameters
+    api_endpoint = "YOUR_GEMINI_API_ENDPOINT"
     headers = {
         "Authorization": f"Bearer {api_key}",
         "Content-Type": "application/json"
     }
     data = {
         "prompt": prompt_parts,
-        "generation_config": generation_config,
-        "safety_settings": safety_settings
+        # Add generation_config and safety_settings as required
     }
-    response = requests.post(api_endpoint, json=data, headers=headers)
-    return response.json()
+    try:
+        response = requests.post(api_endpoint, json=data, headers=headers)
+        return response.json()
+    except requests.RequestException as e:
+        print(e)
+        return None
 
 if __name__ == '__main__':
     app.run(debug=True)
